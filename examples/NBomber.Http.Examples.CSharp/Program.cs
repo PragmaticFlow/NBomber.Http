9f8471af999f1fa3d723a2a5afe15f165c97ca5a
Antya Dev <antyadev@gmail.com>
2018-07-16 01:49:03 +0300

    initial commit

---
+++ examples/NBomber.Http.Examples.CSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using NBomber.Http.Examples.CSharp.Scenarios;
+
+namespace NBomber.Http.Examples.CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var scenario = SimpleScenario.BuildScenario();
+            ScenarioRunner.Run(scenario);
+        }
+    }
+}
---
+++ examples/NBomber.Http.Examples.CSharp/Scenarios/SimpleScenario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NBomber.Http.Examples.CSharp.Scenarios
+{
+    class SimpleScenario
+    {
+        public static Scenario BuildScenario()
+        {
+            var step = HttpRequest.Create("GET", "https://github.com/VIP-Logic/NBomber")
+                                  .BuildStep();
+
+            return new ScenarioBuilder(scenarioName: "SimpleScenario with 100 concurrent users")
+                .AddTestFlow("GET flow", steps: new[] { step }, concurrentCopies: 100)
+                .Build(duration: TimeSpan.FromSeconds(10));
+        }
+    }
+}
