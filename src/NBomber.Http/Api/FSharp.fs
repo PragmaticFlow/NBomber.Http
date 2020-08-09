7cab3158f22e683227d8813669ee688b8296b601
antyadev <antyadev@gmail.com>
2020-08-09 23:29:22 +0300

    added tracing when log is verbose

--- src/NBomber.Http/Api/FSharp.fs
+++ src/NBomber.Http/Api/FSharp.fs
@@ -5,12 +5,13 @@ open System.Net.Http
 open System.Threading.Tasks
 
 open FSharp.Control.Tasks.V2.ContextInsensitive
+open Serilog
+open Serilog.Events
 
 open NBomber
 open NBomber.Contracts
 open NBomber.FSharp
 open NBomber.Http
-open Serilog.Events
 
 module Http =
 
@@ -48,13 +49,34 @@ type HttpStep =
         req.Headers |> Map.iter(fun name value -> msg.Headers.TryAddWithoutValidation(name, value) |> ignore)
         msg
 
+    static member private logRequest (logger: ILogger, req: HttpRequestMessage) =
+
+        let body = if isNull(req.Content) then ""
+                   else req.Content.ReadAsStringAsync().Result
+
+        logger.Verbose("\n [REQUEST]: \n {0} \n [REQ_BODY] \n {1} \n", req.ToString(), body)
+
+    static member private logResponse (logger: ILogger, res: HttpResponseMessage) =
+
+        let body = if isNull(res.Content) then ""
+                   else res.Content.ReadAsStringAsync().Result
+
+        logger.Verbose("\n [RESPONSE]: \n {0} \n [RES_BODY] \n {1} \n", res.ToString(), body)
+
     static member create (name: string, feed: IFeed<'TFeedItem>, createRequest: IStepContext<unit,'TFeedItem> -> Task<HttpRequest>) =
         let client = new HttpClient()
 
         Step.create(name, feed, fun context -> task {
             let! req = createRequest(context)
             let msg = HttpStep.createMsg req
+
+            if context.Logger.IsEnabled(LogEventLevel.Verbose) then
+                HttpStep.logRequest(context.Logger, msg)
+
             let! response = client.SendAsync(msg, HttpCompletionOption.ResponseHeadersRead, context.CancellationToken)
+
+            if context.Logger.IsEnabled(LogEventLevel.Verbose) then
+                HttpStep.logResponse(context.Logger, response)
 
             let responseSize =
                 let headersSize = response.Headers.ToString().Length
@@ -86,7 +108,14 @@ type HttpStep =
         Step.create(name, feed, fun context -> task {
             let req = createRequest(context)
             let msg = HttpStep.createMsg req
+
+            if context.Logger.IsEnabled(LogEventLevel.Verbose) then
+                HttpStep.logRequest(context.Logger, msg)
+
             let! response = client.SendAsync(msg, HttpCompletionOption.ResponseHeadersRead, context.CancellationToken)
+
+            if context.Logger.IsEnabled(LogEventLevel.Verbose) then
+                HttpStep.logResponse(context.Logger, response)
 
             let responseSize =
                 let headersSize = response.Headers.ToString().Length
