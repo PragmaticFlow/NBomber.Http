1a87ee98e614ad5fe305ca23f8b2eb4fc8327526
OleksPal <71185886+OleksPal@users.noreply.github.com>
2025-04-08 14:55:45 +0300

    Change default JsonSerializerOptions from Default to Web (#32)

--- src/NBomber.Http/FSharp.fs
+++ src/NBomber.Http/FSharp.fs
@@ -61,7 +61,7 @@ open NBomber.Http
 
 module Http =
 
-    let mutable GlobalJsonSerializerOptions = JsonSerializerOptions.Default
+    let mutable GlobalJsonSerializerOptions = JsonSerializerOptions.Web
 
     let private getHeadersSize (headers: HttpHeaders) =
         headers
