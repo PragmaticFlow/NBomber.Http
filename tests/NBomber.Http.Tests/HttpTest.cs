55d3aedcaf97cbb83af047eaaf7aaa503c8a731d
User <oleksandr.palamarchuk1310@gmail.com>
2025-04-28 18:14:31 +0300

    Add the http test

---
+++ tests/NBomber.Http.Tests/HttpTest.cs
@@ -0,0 +1,54 @@
+using NBomber.CSharp;
+using NBomberHttp = NBomber.Http.CSharp.Http;
+
+namespace NBomber.Http.Tests;
+
+public class HttpTest
+{
+    [Fact]
+    public void EndToEnd()
+    {
+        // For this example, you'll need to start the HttpApiSimulator, which is located in the examples/simulators solution folder.
+        // Make sure it’s running before executing the client tests to ensure proper communication.
+
+        var clientPool = new ClientPool<HttpClient>();
+
+        var scenario = Scenario.Create("restsharp_scenario", async ctx =>
+        {
+            var client = clientPool.GetClient(ctx.ScenarioInfo);
+            var request = NBomberHttp.CreateRequest("GET", "http://localhost:5071/api/pingpong");
+
+            return await NBomberHttp.Send(client, request);
+        })
+        .WithWarmUpDuration(TimeSpan.FromSeconds(5))
+        .WithLoadSimulations(
+            Simulation.KeepConstant(100, TimeSpan.FromSeconds(5))
+        )
+        .WithInit(async ctx =>
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                var client = new HttpClient();
+                clientPool.AddClient(client);
+            }
+        })
+        .WithClean(ctx =>
+        {
+            clientPool.DisposeClients(client => client.Dispose());
+            return Task.CompletedTask;
+        });
+
+        var stats = NBomberRunner
+            .RegisterScenarios(scenario)
+            .Run();
+
+        Assert.True(stats.AllOkCount > 0);
+        Assert.True(stats.AllFailCount == 0);
+
+        foreach (var scenarioStats in stats.ScenarioStats)
+        {
+            foreach (var stepStats in scenarioStats.StepStats)
+                Assert.True(stepStats.Ok.Latency.MinMs > 0);
+        }
+    }
+}
