f9b2245b506edad151130bcb37efafa1590e886e
User <oleksandr.palamarchuk1310@gmail.com>
2025-04-29 16:53:19 +0300

    Reduce number of KeepConstant copies

--- tests/NBomber.Http.Tests/HttpTest.cs
+++ tests/NBomber.Http.Tests/HttpTest.cs
@@ -74,7 +74,7 @@ public class HttpTest
         })
         .WithoutWarmUp()
         .WithLoadSimulations(
-            Simulation.KeepConstant(10, TimeSpan.FromSeconds(5))
+            Simulation.KeepConstant(2, TimeSpan.FromSeconds(5))
         );
 
         var stats = NBomberRunner
