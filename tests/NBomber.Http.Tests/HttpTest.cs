bfd318f6ab9a7253a4a9339ccc85843b6c3a9388
antyadev <antyadev@gmail.com>
2025-07-12 13:18:36 +0300

    added test for status code

--- tests/NBomber.Http.Tests/HttpTest.cs
+++ tests/NBomber.Http.Tests/HttpTest.cs
@@ -88,7 +88,10 @@ public class HttpTest
         foreach (var scenarioStats in stats.ScenarioStats)
         {
             foreach (var stepStats in scenarioStats.StepStats)
+            {
                 Assert.True(stepStats.Ok.Latency.MinMs > 0);
+                Assert.True(stepStats.Ok.StatusCodes.Length > 0);
+            }
         }
     }
 }
